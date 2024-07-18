6ee0fb174c869c2ca499247644dd3d09d7db639b
Indu <indulahari6@gmail.com>
2024-07-18 14:46:23 +0530

    Add time(in sec) to capture frame/image

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import time
+
 import cv2
+
 
 video = cv2.VideoCapture(0)
 check, frame = video.read()
+time.sleep(1)
+check2, frame2 = video.read()
+time.sleep(1)
+check3, frame3 = video.read()
+time.sleep(1)
 
 print(video)
 print(frame)
